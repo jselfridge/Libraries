a48383ce32df7d1bc1d59e580696972ee96e0414
Justin Selfridge <jselfridge@gmail.com>
2015-03-01 22:55:28 +0000

    Added 'bbbGPIO' to 'bbbLib'.

--- inc/bbbLib.h
+++ inc/bbbLib.h
@@ -13,25 +13,49 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#include <errno.h>
+#include <poll.h>
+#include <stdlib.h>
+#include <string.h>
+
 
 // Custom includes
 #include <matLib.h>
 
 
 // Custom definitions
-#define LED_PATH "/sys/class/leds/beaglebone:green:usr"
-#define MAX_BUF 64
+#define LED_PATH   "/sys/class/leds/beaglebone:green:usr"
+#define GPIO_PATH  "/sys/class/gpio"
+#define MAX_BUF     64
+
+
+// Enumerations
+enum PIN_DIR {
+  INPUT_PIN  = 0,
+  OUTPUT_PIN = 1
+};
+enum PIN_VAL {
+  LOW  = 0,
+  HIGH = 1
+};
 
 
 // bbbLED functions
-int   led_rmtrig  ( unsigned int index );
-int   led_on      ( unsigned int index );
-int   led_off     ( unsigned int index );
-int   led_blink   ( unsigned int index, unsigned int on, unsigned int off );
+int   led_rmtrig     ( unsigned int index );
+int   led_on         ( unsigned int index );
+int   led_off        ( unsigned int index );
+int   led_blink      ( unsigned int index, unsigned int on, unsigned int off );
 
 
 // bbbGPIO functions
-
+void  gpio_export    ( unsigned int gpio );
+void  gpio_unexport  ( unsigned int gpio );
+void  gpio_set_dir   ( unsigned int gpio, enum PIN_DIR dir );
+void  gpio_set_val   ( unsigned int gpio, enum PIN_VAL val );
+void  gpio_get_val   ( unsigned int gpio, unsigned int *val );
+void  gpio_set_edge  ( unsigned int gpio, char *edge );
+void  gpio_fd_open   ( unsigned int gpio );
+void  gpio_fd_close  ( int fd );
 
 
 // bbbUART function
