f4f28c3d4d3bd7e0f3b97ae830799e4c915a9659
jselfridge <jselfridge@gmail.com>
2015-04-06 23:50:39 -0400

    Working on gradient optimization.

--- Debug/OptDebug.c
+++ Debug/OptDebug.c
@@ -14,20 +14,21 @@ void DebugOptLib() {
   printf("\n   --- OptLib Debugging --- \n\n");
 
   // Define optimization parameters
-  double gain = 0.01;
-  double tol  = 0.000001;
+  double gain = 0.001;
+  double tol  = 0.001;
+  int    iter = 0;
 
   // Define initial condition
   matrix* x = mat_init(2,1);
-  mat_set(x,1,1,0.1);
-  mat_set(x,2,1,0.2);
+  mat_set(x,1,1,0.0);
+  mat_set(x,2,1,0.1);
   printf("x: ");
   mat_print(x);
 
   // Define target condition
   matrix* t = mat_init(2,1);
-  mat_set(t,1,1,0.0);
-  mat_set(t,2,1,0.0);
+  mat_set(t,1,1,2.0);
+  mat_set(t,2,1,-3.0);
   printf("t: ");
   mat_print(t);
 
@@ -45,7 +46,7 @@ void DebugOptLib() {
     mat_print(f);
 
     // Error magnitude
-    e = mat_mag( mat_sub(f,t) );
+    e = mat_norm( mat_sub(f,t), 1 );
     printf("e: %1.6f \n", e);
 
     // Revise state
@@ -54,11 +55,14 @@ void DebugOptLib() {
       x = mat_sub( x, mat_scale(f,gain) );
       printf("x: ");
       mat_print(x);
+      iter++;
     }
     else { iterate = 0; }
 
   // Keep looping
   } while ( iterate );
+
+  printf("Iterations: %d", iter);
 
   // Exit program
   printf("\n");
@@ -76,8 +80,8 @@ matrix* F ( matrix* x ) {
   double x1 = mat_get(x,1,1);
   double x2 = mat_get(x,2,1);
 
-  double f1 = x1*x2 + 2*x1 - 3*x2;
-  double f2 = pow(x1,2) - pow(x2,2) + 3*x1*x2;
+  double f1 = x1*x2 + 2*x1 - 3*x2 + 2;
+  double f2 = pow(x1,2) - pow(x2,2) + 3*x1*x2 -3;
 
   mat_set( f,1,1,f1 );
   mat_set( f,2,1,f2 );
