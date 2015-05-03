dfb385af42ef55067bd9cc61147b0fdff2c2bdbd
jselfridge <jselfridge@gmail.com>
2015-05-02 22:02:22 -0400

    Revised syntax for RotLib.h in 'RotLib'.

--- XRotLib/RotLib.h
+++ XRotLib/RotLib.h
@@ -1,10 +1,10 @@
-/*
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//  RotLib.h
+
+//============================================================
+//  RotLib.c
 //  Justin M Selfridge
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#ifndef _ROTLIB_H
-#define _ROTLIB_H
+//============================================================
+#ifndef _ROTLIB_H_
+#define _ROTLIB_H_
 
 
 //  Standard includes
@@ -16,7 +16,7 @@
 #include "../MatLib/MatLib.h"
 
 
-// Precompiled define
+// Define statements
 #define PI  M_PI
 #define PI2 (2.0*PI)
 #define PIH (PI/2.0)
@@ -26,31 +26,30 @@
 
 
 // RotConv functions
-matrix*  rot_d2r( matrix* deg );
-matrix*  rot_r2d( matrix* rad );
-matrix*  rot_wrappi( matrix* rad );
-matrix*  rot_wrap2pi( matrix* rad );
+matrix*  rot_d2r      ( matrix* deg );
+matrix*  rot_r2d      ( matrix* rad );
+void     rot_wrappi   ( matrix* rad );
+void     rot_wrap2pi  ( matrix* rad );
 
 
 // RotEuler functions
-matrix* rot_xaxis  ( double angle );
-matrix* rot_yaxis  ( double angle );
-matrix* rot_zaxis  ( double angle );
-matrix* rot_eul    ( matrix* att );
+matrix*  rot_xaxis    ( double angle );
+matrix*  rot_yaxis    ( double angle );
+matrix*  rot_zaxis    ( double angle );
+matrix*  rot_eul      ( matrix* att  );
 
 
 // RotQuat functions
-matrix* rot_e2q    ( matrix* att );
-matrix* rot_q2e    ( matrix* quat );
-matrix* rot_q2dcm  ( matrix* quat );
-matrix* rot_vec2q  ( matrix* vecA, matrix* vecB );
-matrix* rot_qskew  ( matrix* quat );
-matrix* rot_qmul   ( matrix* quatA, matrix* quatB );
-matrix* rot_qderiv ( matrix* quat );
+matrix*  rot_e2q      ( matrix* att  );
+matrix*  rot_q2e      ( matrix* quat );
+matrix*  rot_q2dcm    ( matrix* quat );
+matrix*  rot_vec2q    ( matrix* vecA, matrix* vecB );
+matrix*  rot_qskew    ( matrix* quat );
+matrix*  rot_qmul     ( matrix* quatA, matrix* quatB );
+matrix*  rot_qderiv   ( matrix* quat );
 
 
 #endif
 
 
 
-*/
