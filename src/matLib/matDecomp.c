8282fcd84bd2ecb0366a420470bc7843ad729a42
jselfridge <jselfridge@gmail.com>
2016-06-28 01:42:55 +0000

    Added 'mat_chol' and 'mat_syminv' to 'matDecomp.c' file.

--- src/matLib/matDecomp.c
+++ src/matLib/matDecomp.c
@@ -148,4 +148,212 @@ double mat_det ( matrix *mat )  {
 }
 
 
+/**
+ *  mat_chol
+ *  Computes the Cholesky factorization of a PDS matrix.  For matrix A, 
+ *  the Cholesky factor U is an upper triangular matrix such that A = U' * U.
+ */
+void mat_chol ( double a[], uint n, uint nn, double u[], uint *nullty, uint *ifault )  {
+
+  double eta = 1.0E-09;
+  uint i;
+  uint icol;
+  uint ii;
+  uint irow;
+  uint j;
+  uint k;
+  uint kk;
+  uint l;
+  uint m;
+  double w;
+  double x;
+
+  *ifault = 0;
+  *nullty = 0;
+
+  if ( n <= 0 )  {
+    *ifault = 1;
+    return;
+  }
+
+  if ( nn < ( n * ( n + 1 ) ) / 2 )  {
+    *ifault = 3;
+    return;
+  }
+
+  j = 1;
+  k = 0;
+  ii = 0;
+
+  // Factorize column by column, ICOL = column number.
+  for ( icol = 1; icol <= n; icol++ )  {
+
+    ii = ii + icol;
+    x = eta * eta * a[ii-1];
+    l = 0;
+    kk = 0;
+
+    // IROW = row number within column ICOL.
+    for ( irow = 1; irow <= icol; irow++ )  {
+
+      kk = kk + irow;
+      k = k + 1;
+      w = a[k-1];
+      m = j;
+
+      for ( i = 1; i < irow; i++ )  {
+        l = l + 1;
+        w = w - u[l-1] * u[m-1];
+        m = m + 1;
+      }
+
+      l = l + 1;
+
+      if ( irow == icol )  {  break;  }
+
+      if ( u[l-1] != 0.0 )  {
+        u[k-1] = w / u[l-1];
+      }
+      else  {
+        u[k-1] = 0.0;
+        if ( fabs ( x * a[k-1] ) < w * w )  {
+          *ifault = 2;
+          return;
+        }
+      }
+
+    }
+
+    // End of row, estimate relative accuracy of diagonal element.
+    if ( fabs ( w ) <= fabs ( eta * a[k-1] ) )  {
+      u[k-1] = 0.0;
+      *nullty = *nullty + 1;
+    }
+
+    else  {
+      if ( w < 0.0 )  {
+        *ifault = 2;
+        return;
+      }
+      u[k-1] = sqrt ( w );
+    }
+
+    j = j + icol;
+
+  }
+
+  return;
+}
+
+
+/**
+ *  mat_syminv
+ *  Computes the inverse of a symmetric PSD matrix.
+ */
+void mat_syminv ( double a[], uint n, double c[], double w[], uint *nullty, uint *ifault )  {
+
+  uint i;
+  uint icol;
+  uint irow;
+  uint j;
+  uint jcol;
+  uint k;
+  uint l;
+  uint mdiag;
+  uint ndiag;
+  uint nn;
+  uint nrow;
+  double x;
+
+  *ifault = 0;
+
+  if ( n <= 0 )  {
+    *ifault = 1;
+    return;
+  }
+
+  nrow = n;
+
+  // Compute the Cholesky factorization of A.
+  // The result is stored in C.
+  nn = ( n * ( n + 1 ) ) / 2;
+  mat_chol( a, n, nn, c, nullty, ifault );
+
+  if ( *ifault != 0 )  return;
+
+  // Invert C and form the product (Cinv)' * Cinv, where Cinv is the inverse
+  // of C, row by row starting with the last row.
+  // IROW = the row number,
+  // NDIAG = location of last element in the row.
+  irow = nrow;
+  ndiag = nn;
+
+  // Special case, zero diagonal element.
+  for ( ; ; )  {
+
+    if ( c[ndiag-1] == 0.0 )  {
+      l = ndiag;
+      for ( j = irow; j <= nrow; j++ )  {
+        c[l-1] = 0.0;
+        l = l + j;
+      }
+    }
+
+    else  {
+      l = ndiag;
+      for ( i = irow; i <= nrow; i++ )  {
+        w[i-1] = c[l-1];
+        l = l + i;
+      }
+      icol = nrow;
+      jcol = nn;
+      mdiag = nn;
+
+      for ( ; ; )  {
+        l = jcol; 
+
+        if ( icol == irow )  {
+          x = 1.0 / w[irow-1];
+        }
+        else  {
+          x = 0.0;
+        }
+
+        k = nrow;
+
+        while ( irow < k )  {
+
+          x = x - w[k-1] * c[l-1];
+          k = k - 1;
+          l = l - 1;
+
+          if ( mdiag < l )  {
+            l = l - k + 1;
+          }
+
+        }
+
+        c[l-1] = x / w[irow-1];
+
+        if ( icol <= irow )  {  break;  }
+
+        mdiag = mdiag - icol;
+        icol = icol - 1;
+        jcol = jcol - 1;
+
+      }
+
+    }
+
+    ndiag = ndiag - irow;
+    irow = irow - 1;
+
+    if ( irow <= 0 )  {  break;  }
+
+  }
+
+  return;
+}
+
+
 
