2a012ca1e6c79e30e2611ee03d753179b59ee606
Justin M Selfridge <jselfridge@users.noreply.github.com>
2020-05-18 09:50:31 -0400

    Update matTemp.h

--- SANDBOX/inc/matLib/matTemp.h
+++ SANDBOX/inc/matLib/matTemp.h
@@ -27,19 +27,7 @@
 
 
 // Function declarations
-float  mat_det    ( matrix* mat );
-float  mat_trace  ( matrix* mat );
-bool   mat_sym    ( matrix* mat, float tol );
-
-
-// Function declarations
-void     mat_LU      ( matrix* mat, matrix* L, matrix* U );
-void     mat_LDU     ( matrix* mat, matrix** L, matrix** D, matrix** U );
-void     mat_QR      ( matrix* mat, matrix** Q, matrix** R );     //---  TODO: QR factorization on short matrices  ---//
-void     mat_chol    ( float a[], uint n, uint nn, float u[], uint* nullty, uint* ifault );
-void     mat_syminv  ( float a[], uint n, float c[], float w[], uint* nullty, uint* ifault );
-matrix*  mat_tri2vec ( matrix* tri );
-matrix*  mat_vec2tri ( matrix* vec );
+void  mat_NEWFUNCTION  ( matrix* mat );
 
 
 #endif
