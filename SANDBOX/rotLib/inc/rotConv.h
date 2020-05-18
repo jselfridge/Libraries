169c497d4d1e872843d04b2eea9fc8dfe8bd120f
Justin M Selfridge <jselfridge@users.noreply.github.com>
2020-05-18 12:37:55 -0400

    Create rotConv.h

---
+++ SANDBOX/rotLib/inc/rotConv.h
@@ -0,0 +1,56 @@
+/*******************************************************************************
+*
+* Justin M Selfridge, PhD
+* Gradient Consulting, LLC
+* jselfridge@gmail.com
+*
+* rotLib.h
+* All inclusive header file for the 'rotLib' library.
+*
+*******************************************************************************/
+#ifndef ROT_LIB_H
+#define ROT_LIB_H
+
+
+// Library header includes
+#include "../inc/rotConv.h"
+#include "../inc/rotEul.h"
+#include "../inc/rotQuat.h"
+
+
+//~~
+
+
+// Standard includes
+//#include <math.h>
+//#include <stdio.h>
+
+
+// Function declarations
+//matrix*  rot_deg2rad  ( matrix *deg );
+//matrix*  rot_rad2deg  ( matrix *rad );
+//void     rot_wrappi   ( matrix *rad );
+//void     rot_wrap2pi  ( matrix *rad );
+
+
+// Function declarations
+//matrix*  rot_xaxis    ( double angle );
+//matrix*  rot_yaxis    ( double angle );
+//matrix*  rot_zaxis    ( double angle );
+//matrix*  rot_eul      ( matrix *att  );
+
+
+// Function declarations
+//matrix*  rot_e2q      ( matrix *att  );
+//matrix*  rot_q2e      ( matrix *quat );
+//matrix*  rot_q2dcm    ( matrix *quat );
+//matrix*  rot_vec2q    ( matrix *vecA, matrix *vecB );
+//matrix*  rot_vec2e    ( matrix *vecA, matrix *vecB );
+//matrix*  rot_qskew    ( matrix *quat );
+//matrix*  rot_qmul     ( matrix *quatA, matrix *quatB );
+//matrix*  rot_qderiv   ( matrix *quat );
+
+
+//#endif
+
+
