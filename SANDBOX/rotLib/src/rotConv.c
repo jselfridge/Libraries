b3f8ddb2047372e9c284dfa1c2f05a7cb314d75f
Justin M Selfridge <jselfridge@users.noreply.github.com>
2020-05-18 13:13:46 -0400

    Update rotConv.c

--- SANDBOX/rotLib/src/rotConv.c
+++ SANDBOX/rotLib/src/rotConv.c
@@ -14,94 +14,57 @@
 
 
 /*******************************************************************************
-* matrix* rot_? ( ? )
-* Description
+* matrix* rot_d2r ( matrix* deg )
+* Converts the angles of a matrix from degrees into radians.
 *******************************************************************************/
-
-
-
-
-
-
-
-
-//============================================================
-//  rotConv.c
-//  Justin M Selfridge
-//============================================================
-//#include "rotLib.h"
-
-
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//  rot_d2r
-//  Converts the angles of a matrix from degrees into radians.
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-/*
-matrix* rot_d2r ( matrix* deg )  {
-  return mat_scale( deg, PI/180.0 );
+matrix* rot_d2r ( matrix* deg ) {
+  return mat_scale( deg, DEG2RAD );
 }
-*/
 
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//  rot_r2d
-//  Converts the angles of matrix from radians into degrees.
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-/*
-matrix* rot_r2d ( matrix* rad )  {
-  return mat_scale( rad, 180.0/PI );
+
+
+
+/*******************************************************************************
+* matrix* rot_r2d ( matrix* rad )  {
+* Converts the angles of matrix from radians into degrees.
+*******************************************************************************/
+matrix* rot_r2d ( matrix* rad ) {
+  return mat_scale( rad, RAD2DEG );
 }
-*/
 
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//  rot_wrappi
-//  Places the elements of a matrix within the range (-pi,pi].
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-/*
-void rot_wrappi ( matrix* rad )  {
 
-  int i, j, r, c;
-  double val;
 
-  r = rad->rows;
-  c = rad->cols;
 
-  for ( i=1; i<=r; i++ ) {
-    for ( j=1; j<=c; j++ ) {
-      val = mat_get(rad,i,j);
-      while ( val >   PI )  val -= PI2;
-      while ( val <= -PI )  val += PI2;
-      mat_set( rad,i,j, val );
-    }
+/*******************************************************************************
+* void rot_wrappi ( matrix* rad )
+* Places the elements of a matrix within the range (-pi,pi].
+*******************************************************************************/
+void rot_wrappi ( matrix* rad ) {
+
+  for( uint i=0; i < sizeof rad / sizeof *rad; i++ ) {
+    while ( *(rad->data+i) >   M_PI )  *(rad->data+i) -= 2.0 * M_PI;
+    while ( *(rad->data+i) <= -M_PI )  *(rad->data+i) += 2.0 * M_PI;
   }
 
   return;
 }
-*/
 
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//  rot_wrap2pi
-//  Places the elements of a matrix within the range [0,2pi).
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-/*
-void rot_wrap2pi ( matrix* rad )  {
 
-  int i, j, r, c;
-  double val;
 
-  r = rad->rows;
-  c = rad->cols;
 
-  for ( i=1; i<=r; i++ ) {
-    for ( j=1; j<=c; j++ ) {
-      val = mat_get(rad,i,j);
-      while ( val >= PI2 )  val -= PI2;
-      while ( val <   0  )  val += PI2;
-      mat_set( rad,i,j, val );
-    }
+/*******************************************************************************
+* void rot_wrap2pi ( matrix* rad )
+* Places the elements of a matrix within the range [0,2pi).
+*******************************************************************************/
+void rot_wrap2pi ( matrix* rad ) {
+
+  for( uint i=0; i < sizeof rad / sizeof *rad; i++ ) {
+    while ( *(rad->data+i) >= 2.0 * M_PI )  *(rad->data+i) -= 2.0 * M_PI;
+    while ( *(rad->data+i) <  0.0        )  *(rad->data+i) += 2.0 * M_PI;
   }
 
   return;
 }
-*/
+
 
 
