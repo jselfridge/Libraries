c5dc14f37e1b1e45294d3dc9580e50e078363823
Justin M Selfridge <jselfridge@users.noreply.github.com>
2020-05-18 12:39:20 -0400

    Update rotLib.h

--- SANDBOX/rotLib/inc/rotLib.h
+++ SANDBOX/rotLib/inc/rotLib.h
@@ -18,40 +18,7 @@
 #include "../inc/rotQuat.h"
 
 
-//~~
-
-
-// Standard includes
-//#include <math.h>
-//#include <stdio.h>
-
-
-// Function declarations
-//matrix*  rot_deg2rad  ( matrix *deg );
-//matrix*  rot_rad2deg  ( matrix *rad );
-//void     rot_wrappi   ( matrix *rad );
-//void     rot_wrap2pi  ( matrix *rad );
-
-
-// Function declarations
-//matrix*  rot_xaxis    ( double angle );
-//matrix*  rot_yaxis    ( double angle );
-//matrix*  rot_zaxis    ( double angle );
-//matrix*  rot_eul      ( matrix *att  );
-
-
-// Function declarations
-//matrix*  rot_e2q      ( matrix *att  );
-//matrix*  rot_q2e      ( matrix *quat );
-//matrix*  rot_q2dcm    ( matrix *quat );
-//matrix*  rot_vec2q    ( matrix *vecA, matrix *vecB );
-//matrix*  rot_vec2e    ( matrix *vecA, matrix *vecB );
-//matrix*  rot_qskew    ( matrix *quat );
-//matrix*  rot_qmul     ( matrix *quatA, matrix *quatB );
-//matrix*  rot_qderiv   ( matrix *quat );
-
-
-//#endif
+#endif
 
 
 
