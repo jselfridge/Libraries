65e8355b6084ae75dada2bc9c6d12acee0533c2d
Justin M Selfridge <jselfridge@users.noreply.github.com>
2020-05-20 13:49:36 -0400

    Update matArith.h

--- matLib/inc/matArith.h
+++ matLib/inc/matArith.h
@@ -18,24 +18,24 @@
 
 // Library includes
 #include "matCommon.h"
-#include "matManip.h"
-#include "matDecomp.h"
+// #include "matManip.h"
+// #include "matDecomp.h"
 
 
 // Function declarations
-matrix*  mat_add      ( matrix* matA, matrix* matB );
-matrix*  mat_sub      ( matrix* matA, matrix* matB );
-matrix*  mat_emul     ( matrix* matA, matrix* matB );
-matrix*  mat_ediv     ( matrix* matA, matrix* matB );
-matrix*  mat_mul      ( matrix* matA, matrix* matB );
-matrix*  mat_inv      ( matrix* mat );
-matrix*  mat_divL     ( matrix* matA, matrix* matB );
-matrix*  mat_divR     ( matrix* matA, matrix* matB );
-matrix*  mat_epow     ( matrix* mat, uint power );
-matrix*  mat_pow      ( matrix* mat, uint power );
-matrix*  mat_abs      ( matrix* mat );
-matrix*  mat_trans    ( matrix* mat );
-matrix*  mat_reshape  ( matrix* mat, uint rows, uint cols );
+matrix*  mat_add      ( matrix* A, matrix* B );
+matrix*  mat_sub      ( matrix* A, matrix* B );
+matrix*  mat_emul     ( matrix* A, matrix* B );
+matrix*  mat_ediv     ( matrix* A, matrix* B );
+matrix*  mat_mul      ( matrix* A, matrix* B );
+matrix*  mat_inv      ( matrix* M );
+matrix*  mat_divL     ( matrix* A, matrix* B );
+matrix*  mat_divR     ( matrix* A, matrix* B );
+matrix*  mat_epow     ( matrix* M, uint p );
+matrix*  mat_pow      ( matrix* M, uint p );
+matrix*  mat_abs      ( matrix* M );
+matrix*  mat_trans    ( matrix* M );
+matrix*  mat_reshape  ( matrix* M, uint r, uint c );
 
 
 #endif
